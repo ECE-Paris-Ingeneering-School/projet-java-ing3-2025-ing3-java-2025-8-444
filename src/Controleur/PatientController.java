ccf1700cee23267d9dee8b429fde9c02bb937423
Marie <marie.lignoncoscioli@edu.ece.fr>
2025-04-15 19:56:18 +0200

    incription refaite

--- src/Controleur/PatientController.java
+++ src/Controleur/PatientController.java
@@ -8,6 +8,13 @@ public class PatientController {
     public boolean inscrirePatient(String nom, String prenom, String email, String motDePasse) {
         Patient patient = new Patient(0, nom, prenom, email, motDePasse);
         return DAOFactory.getPatientDAO().save(patient);
+    }
 
+    public Patient getPatientByEmail(String email) {
+        return DAOFactory.getPatientDAO().getAll()
+                .stream()
+                .filter(p -> p.getEmail().equalsIgnoreCase(email))
+                .findFirst()
+                .orElse(null);
     }
 }
