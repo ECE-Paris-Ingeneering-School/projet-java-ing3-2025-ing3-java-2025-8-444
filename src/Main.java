8951a8200b6ffe1ddffb37045d052a3478f21872
Léopold <116790573+yaplusaucunpseudodelibre@users.noreply.github.com>
2025-04-15 13:02:22 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import view.ConnexionView;
+import view.AccueilView;
 
 public class Main {
     public static void main(String[] args) {
-        new ConnexionView();
+        new AccueilView();
     }
 }
