d67b54ba9696ba266b35b7c07d74c60f4be8eb05
Prinzspin <thomas.spinner0@gmail.com>
2025-04-18 13:56:23 +0200

    On initialise les exeptions

---
+++ src/util/ExeptionConstante.java
@@ -0,0 +1,12 @@
+package util;
+
+public class ExeptionConstante {
+
+    //AdesseNotFoundException, avec ou sans les parameterises
+    public static final String ADRESSE_NOT_FOUND = "Adresse not found.";
+    public static final String ADRESSE_NOT_FOUND_WITH_ID = "Adresse not found for id %s.";
+
+    //catagorieNotFoundException, avec ou sans les parameterises
+    public static final String CATEGORIES_NOT_FOUND = "Categories not found";
+
+}
