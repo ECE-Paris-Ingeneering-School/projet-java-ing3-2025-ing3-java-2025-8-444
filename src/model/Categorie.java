319fe7acaf7c27633f02aa7c40e4c43055e27854
Prinzspin <thomas.spinner0@gmail.com>
2025-04-18 11:51:45 +0200

    nouvelle classe

--- src/model/Categorie.java
+++ src/model/Categorie.java
@@ -5,6 +5,36 @@ public class Categorie {
     private String nom;
     private Specialiste[] specialiste;
 
-    
+    public Categorie(Integer numero, String nom, Specialiste[] specialiste) {
+        this.numero = numero;
+        this.nom = nom;
+        this.specialiste = specialiste;
+    }
 
+    public Categorie() {
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Specialiste[] getSpecialiste() {
+        return specialiste;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setSpecialiste(Specialiste[] specialiste) {
+        this.specialiste = specialiste;
+    }
 }
