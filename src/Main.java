a9ff737c3b6bd1db22d90f5d991624f767ea6e34
Marie <marie.lignoncoscioli@edu.ece.fr>
2025-04-27 20:25:52 +0200

    javadoc main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 import view.AcceuilView;
 
+/**
+ * Classe principale du programme.
+ * Lance l'application en affichant la vue d'accueil.
+ */
+
 public class Main {
+
+    /**
+     * Démarre l'application en créant une instance de {@link view.AcceuilView}.
+     *
+     * @param args Les arguments de la ligne de commande
+     */
     public static void main(String[] args) {
         new AcceuilView();
     }
